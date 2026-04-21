c9b7e1968f198d88292b399a310b09a5f06bedae
Vasken Poitras <6317374@johnabbottcollege.net>
2026-04-21 12:29:08 -0400

    Commiting frontend

---
+++ MileStone6/AddCategory.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows;
+
+namespace MileStone6
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class AddCategory : Window
+    {
+        public AddCategory()
+        {
+            InitializeComponent();
+        }
+
+        private void CancelButton_CLicked(object sender, RoutedEventArgs e)
+        {
+            MainWindow mw = new MainWindow();
+            mw.Show();
+            this.Close();
+        }
+    }
+}
