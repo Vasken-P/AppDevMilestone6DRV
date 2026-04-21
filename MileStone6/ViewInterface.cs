6a87630f13c703a11a1599a85c4e3d118f970031
Rebecca <foo@com>
2026-04-21 12:59:39 -0400

    Added the view class and made the methods

---
+++ MileStone6/ViewInterface.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MileStone6
+{
+    internal interface ViewInterface
+    {
+        void ShowMessage(string message);
+        string GetDescription();
+        void ClearFields();
+        void Close();
+    }
+}
