735267a2f65f10f03a3167e32c5df86b62860328
Rebecca <foo@com>
2026-04-29 23:19:22 -0400

    trying to fix errors

--- MileStone6/MainWindow.xaml.cs
+++ MileStone6/MainWindow.xaml.cs
@@ -8,18 +8,54 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using MileStone6Presenter;
+
 
 namespace MileStone6Presenter
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window 
+    public partial class MainWindow : Window , ViewInterface
     {
-        
+        private Presenter p;
         public MainWindow()
-        {   
+        {
             InitializeComponent();
+            p = new Presenter(this);
+        }
+
+        //public void GetFolderName()
+        //{
+        //     System.Windows.Forms.FolderBrowserDialog dialog = new System.Windows.Forms.FolderBrowserDialog()
+            
+              
+        //        if (dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+        //        {
+        //            p.FolderChosen(dialog.SelectedPath);
+        //        }
+            
+        //}
+
+        public void GetFileName()
+        {
+            var dlg = new Microsoft.Win32.SaveFileDialog();
+            dlg.Filter = "Database (*.db)|*.db";
+            if (dlg.ShowDialog() == true)
+            {
+                string name = System.IO.Path.GetFileName(dlg.FileName);
+                p.FileChosen(name);
+            }
+        }
+
+        public void DisplayCurrentFile(string filePath)
+        {
+            MessageBox.Show("File chosen: " + filePath);
+        }
+
+        private void ChooseFileButton_Click(object sender, RoutedEventArgs e)
+        {
+            p.ChooseFolder();
         }
 
         public void ClearCategoryFields()
